5e4d2f92540c8dfa93f44eb2ea918f9d8abbacdc
lucasgcaldas <lggomes007@gmail.com>
2022-04-14 20:53:31 -0300

    Creating branch

--- main.c
+++ main.c
@@ -34,71 +34,64 @@ int main()
 {
     FILE *arq;
     CsvNums vet;
-    int opc;
+    int opc = 0;
     char stp, arqName[30];
     No *head = NULL;
 
-    do
+
+    while(opc != 4)
     {
-        system("clear || cls");
         menu();
         scanf("%d", &opc);
-
+        system("clear || cls");
         switch (opc)
         {
-        case 1:
-            system("clear || cls");
-
-            printf("Digite o nome do arquivo a ser usado:\n\n");
-            scanf("%s", arqName);
-
-            arq = fopen(arqName, "r");
-
-            if (arq == NULL)
-            {
+            case 1:
                 system("clear || cls");
-                printf("Erro ao abrir o arquivo!\n\n");
+
+                printf("Digite o nome do arquivo a ser usado:\n\n");
+                scanf("%s", arqName);
+
+                arq = fopen(arqName, "r");
+
+                if (arq == NULL)
+                {
+                    system("clear || cls");
+                    printf("Erro ao abrir o arquivo!\n\n");
+                    system("pause");
+                    break;
+                }
+
+                vet = getNums(arq, head);
+
+                fclose(arq);
+
+                for (size_t i = 0; i < vet.qtdNum; i++)
+                {
+                    head = inserirNums(head, vet.nums[i]);
+                }
+
                 system("pause");
                 break;
-            }
+            case 2:
 
-            vet = getNums(arq, head);
+                printf("%d\n", head->Chave);
+                printf("%d\n", head->pDir->Chave);
+                printf("%d\n", head->pEsq->Chave);
 
-            fclose(arq);
+                break;
+            case 3:
+                imprimir(head);
+                break;
+            case 4:
 
-            for (size_t i = 0; i < vet.qtdNum; i++)
-            {
-                // num = arvore(vet[10]);
-                head = inserirNums(head, vet.nums[i]);
-            }
-
-            system("pause");
-            break;
-        case 2:
-
-            printf("%d\n", head->Chave);
-            printf("%d\n", head->pDir->Chave);
-            printf("%d\n", head->pEsq->Chave);
-
-            break;
-        case 3:
-            imprimir(head);
-            break;
-        case 4:
-
-            free(head);
-            free(vet.nums);
-            break;
-        default:
-            // system("clear || cls");
-
-            if (opc != 4)
-            {
+                free(head);
+                free(vet.nums);
+                break;
+            default:
                 printf("opcao invalida");
-            }
-            break;
         }
-    } while (opc != 4);
+    }
 
     return 0;
 }
